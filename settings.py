9cc7b6796ff9814a4ee22a40f31a263beaf40c50
Marty <saint@ghost.lt>
2024-09-27 23:37:48 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -24,7 +24,7 @@ ALLOW_UPLOADS = os.environ.get("ALLOW_UPLOADS")
 
 S3_ENABLED = os.environ.get("S3_ENABLED")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY: os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
 AWS_ENDPOINT_URL = os.environ.get("AWS_ENDPOINT_URL")
 
