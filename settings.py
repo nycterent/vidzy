cbeae60cf83cdb387953ccdcd3556b242511994f
Honjes <honjes@protonmail.com>
2025-01-08 21:31:43 +0100

    add an conversion too bolean

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 
 # BELOW CODE IS FOR PRODUCTION ONLY
-#project_folder = os.path.expanduser('~/mysite')  # adjust as appropriate
-#load_dotenv(os.path.join(project_folder, '.env'))
+# project_folder = os.path.expanduser('~/mysite')  # adjust as appropriate
+# load_dotenv(os.path.join(project_folder, '.env'))
 # ABOVE CODE IS FOR PRODUCTION ONLY
 
 load_dotenv('.env')
@@ -17,12 +17,12 @@ MYSQL_CURSORCLASS = "DictCursor"
 
 SECRET_KEY = os.environ.get("VIDZY_APP_SECRET")
 
-MINIFY_HTML = os.environ.get("MINIFY_HTML")
+MINIFY_HTML = os.environ.get("MINIFY_HTML") == "True"
 HOST = os.environ.get("HOST")
 
 ALLOW_UPLOADS = os.environ.get("ALLOW_UPLOADS")
 
-S3_ENABLED = os.environ.get("S3_ENABLED")
+S3_ENABLED = os.environ.get("S3_ENABLED") == "True"
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
@@ -30,6 +30,4 @@ AWS_ENDPOINT_URL = os.environ.get("AWS_ENDPOINT_URL")
 S3_PUBLIC_URL = os.environ.get("S3_PUBLIC_URL")
 
 SENTRY_DSN = os.environ.get("SENTRY_DSN")
-SENTRY_ENABLED = os.environ.get("SENTRY_ENABLED")
-
-
+SENTRY_ENABLED = os.environ.get("SENTRY_ENABLED") == "True"
